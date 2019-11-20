5800cc92cd95677562a6a3dccd97459b5c7b7563
João Santos <joao.santos@bitflow.pt>
2019-11-20 11:36:57 +0000

    adicionado helloworld do flask

---
+++ canteen/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+from flask import abort
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def mainPage():
+    return "Hello world!"
+
+if __name__ == '__main__':
+    app.run(port=8080)
