4cee2b3d81ce480f453464529b23de2820a458c3
AndreGodinho07 <andregodinho06@gmail.com>
2019-11-27 14:34:32 +0000

    Edits recommended by João

--- rooms/main.py
+++ rooms/main.py
@@ -9,21 +9,7 @@ import roomDB
 app = Flask(__name__)
 db = roomDB.RoomDB("IST Rooms")
 
-def get_building(node, URI = "https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/"):
-    """
-    Gets the building of the room by recursively accessing parents info 
-    """
-    if node["parentSpace"]["type"] == 'BUILDING':
-        return node["parentSpace"]["name"]
-
-    else:
-        id_parent = node["parentSpace"]["id"]
-        URI += id_parent
-        r = requests.get(URI)
-        node = r.json()
-        building = get_building(node)
-
-    return building
+URI_room = "https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/"
 
 def notFound():
     resp = jsonify(error = "Room, not found.")
@@ -39,40 +25,16 @@ def listRooms():
     return resp
 
 @app.route('/getRoom/<identifier>', methods=['GET'])
-def getRoom(identifier, URI = "https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces/"):
+def getRoom(identifier):
     r_id = str(identifier)
 
-    try:
+    try: 
         room = db.showRoom(r_id).__dict__
-    
-    except AttributeError:
-        URI += str(identifier)
-        r = requests.get(URI)
-        room = r.json()
-        
-        if "error" in room.keys():
-            if room["error"] == "id not found":
-                return notFound()
-        
-        building = get_building(room)
-            
-        if not room["events"]: # if wrong identifier is inserted
-            timetable = None
-        else: 
-            timetable = room["events"]
-
-        room = db.createRoom(room["name"],
-                                room["id"],
-                                room["topLevelSpace"]["name"],
-                                building,
-                                timetable)
-
-        resp = jsonify(room.__dict__)
-        resp.statusCode = 201
-        
-    else:
         resp = jsonify(room)
         resp.statusCode = 200
+    
+    except AttributeError: 
+        resp = notFound()
 
     return resp
 
