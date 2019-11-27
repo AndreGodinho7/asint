2e8657641b5cca6a3e0737e608e244060807fd86
Diogo Oliveira <diogo.jordao@tecnico.ulisboa.pt>
2019-11-27 10:53:54 +0000

    não ligar

--- secretariats/main.py
+++ secretariats/main.py
@@ -2,12 +2,18 @@ from flask import Flask
 from flask import request
 from flask import abort
 from flask import jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def mainPage():
-    return "Hello world!"
+def apiListMenus():
+    URL = "https://fenixedu.org/dev/api/#get-canteen"
+    r = requests.get(url = URL)
+    data = r.json()
+    print(data)
+
+    #return "Hello world!"
 
 if __name__ == '__main__':
     app.run(port=8082)
