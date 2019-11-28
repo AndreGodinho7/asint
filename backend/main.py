8e3d0c462df90be0b6981b1f1853dc0dee43b9fb
AndreGodinho07 <andregodinho06@gmail.com>
2019-11-28 02:08:14 +0000

    route showBook

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,33 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 from flask import abort
 from flask import jsonify
+import microservices
 
 app = Flask(__name__)
+
+api = microservices.Microservices()
 
 @app.route("/")
 def mainPage():
     return "Hello world!"
 
+@app.route('/api/room/<identifier>', methods=['GET'])
+def showRoom(identifier):
+    r_id = int(identifier)
+
+    try:
+        room = api.getRoom(r_id)
+
+    except microservices.NotFoundErrorException:
+        return render_template("errorPage.html", id = request.args["Id"])
+
+    except microservices.ServerErrorException:
+        return render_template("servererrorPage.html")
+    
+    else:
+        return render_template("showRoom.html", chosen_room = room, all= room['timetable'])
+
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(port=8089)
