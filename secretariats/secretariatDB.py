6ba4a1772495558596746625f4e2e988f5f69179
João Santos <jbsants@me.com>
2019-12-09 00:27:31 +0000

    correção de bug em secretariat; inserção da classe secretariat no ficheiro da classe secretariat db

--- secretariats/secretariatDB.py
+++ secretariats/secretariatDB.py
@@ -1,5 +1,12 @@
 import pickle
-from secretariat import Secretariat
+
+class Secretariat:
+    def __init__(self, id, location, name, description, opening_hours):
+        self.id = id
+        self.location = location
+        self.name = name
+        self.description = description
+        self.opening_hours = opening_hours
 
 class SecretariatDB:
     dumpFile = "secretariatDump.db"
